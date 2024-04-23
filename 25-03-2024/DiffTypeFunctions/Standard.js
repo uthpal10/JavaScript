3b8ff3708e0f8f4f02d99be38cd0d1220db645eb
uthpal10 <uthpal.ramesh10@gmail.com>
2024-04-23 16:43:08 +0530

    as

--- 25-03-2024/DiffTypeFunctions/Standard.js
+++ 25-03-2024/DiffTypeFunctions/Standard.js
@@ -1,11 +1,13 @@
-// Standard function
+// Standard function 
+// Hoisting is possible
 
+add(8,6)
 function add(a,b) 
 {
-    // let c = a+b;
-    // console.log(c);
-    console.log(arguments);
-    return a+b;
+    let c = a+b;
+    console.log(c);
+    // console.log(arguments);
+    // return a+b;
 }
 // add(8,6)
-console.log(add(3,6));
+// console.log(add(3,6));
