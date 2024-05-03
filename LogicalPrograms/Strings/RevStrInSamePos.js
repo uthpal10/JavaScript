ba9b7477c417f89a29f5de8284ec42cb5a1f77d4
uthpal10 <uthpal.ramesh10@gmail.com>
2024-05-03 07:17:16 +0530

    nh

--- LogicalPrograms/Strings/RevStrInSamePos.js
+++ LogicalPrograms/Strings/RevStrInSamePos.js
@@ -1,3 +1,17 @@
 let str = "JavaScript is scripting language"
+console.log(str);
 
-let splitted = str.split()
+let splitted = str.split(" ");
+
+for(let i=0; i<splitted.length; i++)
+{
+    let st = splitted[i], rev = "";
+
+    for(let j=st.length-1; j>=0; j--)
+    {
+        // rev = rev + st.charAt(j);
+        rev = rev + st[j];
+    }
+    process.stdout.write(rev + " ");
+    // console.log(rev);
+}
