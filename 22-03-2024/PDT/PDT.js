9d10da65c2d0cdff6ff855bf4252f07a876aede6
uthpal10 <uthpal.ramesh10@gmail.com>
2024-05-05 12:14:22 +0530

    z

--- 22-03-2024/PDT/PDT.js
+++ 22-03-2024/PDT/PDT.js
@@ -3,12 +3,14 @@ let a;
 console.log(a);
 a = 'Now it is defined'
 console.log(a);
+console.log(typeof(a));
 
 // number
 let b = 20
 console.log(b);
 b = 20.20
-console.log(b); 
+console.log(b);
+console.log(typeof(b)); 
 
 // string
 const c = 'uthpal'
@@ -19,9 +21,12 @@ let d = true
 console.log(d);
 d = false
 console.log(d);
+console.log(typeof(d));
 
 // null
 let e = null;
 console.log(e);
+console.log(typeof(null));
+
 e = 'ramesh'
 console.log(e);
