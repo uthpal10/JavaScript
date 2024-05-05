6dc30bf4f9d7731cb33f8f7e50271ec3be118135
uthpal10 <uthpal.ramesh10@gmail.com>
2024-05-05 12:35:35 +0530

    m

--- 22-03-2024/Loops/forIn.js
+++ 22-03-2024/Loops/forIn.js
@@ -1,30 +1,28 @@
 // It used to iterate over an enumerable (countable or listable) (that can be iterated) property of an object
-
 // used in objects
-
 // returns properties (keys) of an object
 
-// const laptop = 
-// {
-//     brand : 'lenovo', 
-//     model : 'Z51-70', 
-//     cost : [55000,30000], 
+const laptop = 
+{
+    brand : 'lenovo', 
+    model : 'Z51-70', 
+    cost : [55000,30000], 
 
-//     details : function ()
-//     {
-//         console.log(`The Laptop details are ${this.brand} ${this.model} ${this.cost}`);
-//     }    
-// }
-// laptop.details();
-// console.log(laptop);
+    details : function ()
+    {
+        console.log(`The Laptop details are ${this.brand} ${this.model} ${this.cost}`);
+    }    
+}
+laptop.details();
+console.log(laptop);
 
 let arr = [12, 2, 3];
 
 for (const key in arr) 
 {
-    // console.log(`${i} --> ${arr[i]}`);
-
-    console.log(arr[key]);
+    console.log(`${key} --> ${arr[key]}`);
+    // console.log(key);
+    // console.log(arr[key]);
 
 }
 
