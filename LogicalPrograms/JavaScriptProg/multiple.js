0c513bc5d7723b5bb26a064a901652741b28f93e
uthpal10 <uthpal.ramesh10@gmail.com>
2024-05-11 11:37:23 +0530

    fdjg

--- LogicalPrograms/JavaScriptProg/multiple.js
+++ LogicalPrograms/JavaScriptProg/multiple.js
@@ -1,13 +1,16 @@
-function multiple(num)
-{
-    if(num % 5 ===0 && num % 10 === 0)
-        {
-            console.log(`${num} is a multiple of 5 and 10`);
-        }
-        else
-        {
-            console.log(`${num} is not a multiple of 5 and 10`);
-        }
-}
-const input = 70;
-multiple(input);
+// function multiple(num)
+// {
+//     if(num % 5 ===0 && num % 10 === 0)
+//         {
+//             console.log(`${num} is a multiple of 5 and 10`);
+//         }
+//         else
+//         {
+//             console.log(`${num} is not a multiple of 5 and 10`);
+//         }
+// }
+// const input = 70;
+// multiple(input);
+
+let no = 70;
+console.log(no%5==0 || no%10==0 ? `${no}` : `Not Divisible`);
