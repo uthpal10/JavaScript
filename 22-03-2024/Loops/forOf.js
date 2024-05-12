8cac8a3714e260730db1a1489bb946958ed0c45f
uthpal10 <uthpal.ramesh10@gmail.com>
2024-05-12 09:35:27 +0530

    a

--- 22-03-2024/Loops/forOf.js
+++ 22-03-2024/Loops/forOf.js
@@ -1,19 +1,41 @@
-// It iterates over values of an iterable objects
-// used with Array, Strings, Maps, sets
+// It iterates over values of an iterable objects <==> can be used with Array, Strings, Maps, sets
 // Returns the values of iterable objects 
 
+// forOf in the Array
 const abb = ['hello', 12];
+for (let key of abb)
+{
+   console.log(key);
+}
 
+// forOf in the Map
 let map = new Map();
 
 // To add elements into the map
 map.set('Uthpal', 'Engineer'); 
 map.set('Cricket', 'Sport');
 
-// To get the values from map  
 console.log(map.get('Uthpal'));
 
-for (const key in abb) 
+// To get the values from map  ==> for of returns the map elements in the form of array.
+for (const key of map) 
 {
-   console.log(key+ " : " +abb[key]);
+   console.log(key);
+}
+
+// forOf in the Object
+// Alone we can't fetch the values from the object using forOf loop ==> we will get the error Not iterable
+// ==> we should use object.entries() for that.
+const Cricket = 
+{
+   name : 'Virat',
+   country : 'India'
+};
+
+console.log("-------------------------")
+
+let acc = Object.entries(Cricket);
+for(let key of acc)
+{
+   console.log(key);
 }
---
+++ 22-03-2024/Loops/maps.js
@@ -0,0 +1,13 @@
+let map = new Map();
+
+// To add elements into the map
+map.set('Uthpal', 'Engineer'); 
+map.set('Cricket', 'Sport');
+
+// To get the values from map  
+console.log(map.get('Uthpal'));
+
+for (const key in abb) 
+{
+   console.log(key+ " : " +abb[key]);
+}
